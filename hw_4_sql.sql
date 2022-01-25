1375d34549641b3fd06c77c10d01efdbac60a048
Anton <fox_toha@mail.ru>
2022-01-26 03:25:16 +0500

    change file

--- hw_4_sql.sql
+++ hw_4_sql.sql
@@ -1,19 +1,13 @@
  
  
-2.	 
-select tc.table_name, tc.constraint_name, tc.constraint_type,
-kcu.column_name 
-from information_schema.table_constraints tc 
-left join information_schema.key_column_usage kcu on tc.table_name = kcu.table_name
-and tc.constraint_name = kcu.constraint_name 
-where tc.constraint_type = 'PRIMARY KEY'
- 
- 
+2.	Â 
+
+Â 
 select *
 from public.customer
 where active = '0'
  
- 
+Â 
 select*
 from public.film
 where release_year = 2006
